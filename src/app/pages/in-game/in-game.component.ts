71f58062466877886a03b780993289d7c4f3e978
Anisdhiyaeddine BOUDIAF <anisdhiyaeddine.bondiaf@amadeus.com>
2023-02-01 15:26:45 +0100

    Correctif

--- src/app/pages/in-game/in-game.component.ts
+++ src/app/pages/in-game/in-game.component.ts
@@ -1,6 +1,8 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Route } from '@angular/router';
 import { game, get_unachieved_game, player, register_score } from 'design/function-definitions';
+
+
 @Component({
   selector: 'app-in-game',
   templateUrl: './in-game.component.html',
@@ -19,6 +21,11 @@ export class InGameComponent implements OnInit {
   currentPlayerIndex = 0;
 
 
+  /////////////////////////////////////////////////////////////////////
+
+  state: any = {};
+ 
+
 
   constructor(
     private route: ActivatedRoute,
@@ -30,19 +37,14 @@ export class InGameComponent implements OnInit {
     this.route.params.subscribe((params) => {
       this.game = JSON.parse(params['data']);
     });
-    // this.game.players.forEach((p) => {
-    //   for (let i = 0; i < this.game.rounds; i++) {
-    //     p.scores.push({
-    //       first_shoot: 0,
-    //       second_shoot: 0,
-    //       third_shoot: 0,
-    //       total: 0
-    //     });
-    //   }
-    // });
-    console.log(this.game);
-    
 
+    this.game.players.forEach((player) => {
+      this.state[player.name] = {};
+      for (let i = 0; i < this.game.rounds - 1; i++) {
+        this.state[player.name][i] = [-1, -1];
+      }
+      this.state[player.name][this.game.rounds - 1] = [-1, -1, -1];
+    });
   }
 
   onScoreChange(player: player, round: number, property: string, event: any): void {
@@ -50,57 +52,23 @@ export class InGameComponent implements OnInit {
     console.log("1");
   }
   
-  register_score_tour(){
-    // this.game.players[0].scores.push({
-    //   first_shoot: this.selectedNbPins,
-    //   second_shoot: 0,
-    //   third_shoot: 0,
-    //   total: 0
-    // });
-
-    while(this.currentPlayerIndex !== this.game.players.length && this.currentTour !== this.game.rounds){
-      console.log("debut index : " + this.currentPlayerIndex);
-      if (this.currentTour > this.game.rounds) {
+  register_score_tour() {
+    for (let round = 0; round < this.game.rounds; round++) {
+      for (let player in this.state) {
+        if(!this.state[player][round].includes(-1)) {
+          continue;
+        }
+        for(let shoot = 0; shoot < this.state[player][round].length; shoot++) {
+          if(this.state[player][round][shoot] === -1) {
+            this.state[player][round][shoot] = this.selectedNbPins;
+            return;
+          }
+        }
         return;
       }
-    
-      // const currentPlayer = this.game.players[currentPlayerIndex];
-    
-      if (!this.game.players[this.currentPlayerIndex].scores[this.currentTour - 1]) {
-        console.log("2");
-        this.game.players[this.currentPlayerIndex].scores[this.currentTour - 1] = { first_shoot: -1, second_shoot: -1, total: 0};
-        console.log(this.game.players);
-        
-
-      }
-    
-      if (this.game.players[this.currentPlayerIndex].scores[this.currentTour - 1].first_shoot === -1 && this.game.players[this.currentPlayerIndex].scores[this.currentTour - 1].second_shoot === -1) {
-        console.log("3");
-        // Demander au joueur de renseigner son first_shoot pour ce tour
-        this.game.players[this.currentPlayerIndex].scores[this.currentTour - 1].first_shoot = this.selectedNbPins;
-        console.log(this.game.players);
-
-
-      } else if (this.game.players[this.currentPlayerIndex].scores[this.currentTour - 1].second_shoot === -1 && this.game.players[this.currentPlayerIndex].scores[this.currentTour - 1].first_shoot !== -1) {
-        console.log("4");
-        // // Demander au joueur de renseigner son second_shoot pour ce tour
-        // this.game.players[this.currentPlayerIndex].scores[this.currentTour - 1].second_shoot = this.selectedNbPins;
-        // console.log(this.game.players);
-        
-
-      } else if (this.game.players[this.currentPlayerIndex].scores[this.currentTour - 1].second_shoot !== -1 && this.game.players[this.currentPlayerIndex].scores[this.currentTour - 1].first_shoot !== -1) {
-        console.log("5");
-        // Le joueur a renseigné ses scores pour ce tour, passer au joueur suivant
-        // currentPlayerIndex = (currentPlayerIndex + 1) % this.game.players.length;
-
-        // currentPlayerIndex += 1;
-        console.log("index maj : " + this.currentPlayerIndex);
-        this.currentTour += 1;
-      }
     }
-    this.game.players = [...this.game.players];
-    this.cd.detectChanges();
-    
+
+    console.log(this.state);
   }
 
   submitForm() {
